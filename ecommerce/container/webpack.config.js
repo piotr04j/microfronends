fdf9e896d28acc7332118dd6ddb12e5fcf289776
piotr04j <darricck04@gmail.com>
2022-10-02 20:31:00 +0200

    copied build setup: ts, babel, eslint

--- ecommerce/container/webpack.config.js
+++ ecommerce/container/webpack.config.js
@@ -1,13 +1,35 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const isDevelopment = process.env.NODE_ENV === 'development'
 
 module.exports = {
-  mode: 'development',
+  mode: isDevelopment ? 'development' : 'production',
   devServer: {
     port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+  },
+  output: {
+    filename: '[hash].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins:[
     new HtmlWebpackPlugin({
       template: './public/index.html'
-    })
+    }),
+    new CleanWebpackPlugin(),
   ]
 }
