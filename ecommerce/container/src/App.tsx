8352bd6771c6359cb204090d09cf9684172ec30c
piotr04j <darricck04@gmail.com>
2022-10-16 12:46:41 +0200

    adjusted container to handle dashboard app

--- ecommerce/container/src/App.tsx
+++ ecommerce/container/src/App.tsx
@@ -1,22 +1,28 @@
 import Header from './components/Header'
 import { createGenerateClassName, StylesProvider } from '@material-ui/core'
 import { BrowserRouter, Route, Routes, } from 'react-router-dom';
-import AuthApp from './components/AuthApp';
-import MarketingApp from './components/MarketingApp';
+import { lazy, Suspense } from 'react';
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'con'
 })
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'))
+const AuthLazy = lazy(() => import('./components/AuthApp'))
+const DashboardLazy = lazy(() => import('./components/DashboaradApp'))
 
 const App: () => JSX.Element = () => {
   return (
         <BrowserRouter>
             <StylesProvider generateClassName={generateClassName}>
                 <Header signedIn={false} onSignOut={true}/>
-                <Routes>
-                    <Route path={'/auth/*'} element={<AuthApp />} />
-                    <Route path={'*'} element={<MarketingApp />} />
-                </Routes>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Routes>
+                        <Route path={'/auth/*'} element={<AuthLazy />} />
+                        <Route path={'/dashboard'} element={<DashboardLazy />} />
+                        <Route path={'*'} element={<MarketingLazy />} />
+                    </Routes>
+                </Suspense>
             </StylesProvider>
         </BrowserRouter>
   )
